ebad3ff529cbfb372b434c33ef3dd11ccc97189a
MrEmmanuel <sejesoo@gmail.com>
2022-02-23 01:27:28 +0200

    modified some files

--- src/test/java/springbootpart1/springboot/WebSecurityConfigurerTest.java
+++ src/test/java/springbootpart1/springboot/WebSecurityConfigurerTest.java
@@ -6,22 +6,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SecuredControllerRestTemplateIntegrationTest {
+public class WebSecurityConfigurerTest {
 
     @Autowired
     private TestRestTemplate template;
 
 
     @Test
-    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception {
+    public void securityTest() throws Exception {
         ResponseEntity<String> result = template.withBasicAuth("spring", "secret")
-                .getForEntity("/private/hello", String.class);
-        assertEquals(HttpStatus.OK, result.getStatusCode());
+                .getForEntity("/user", String.class);
+                result.getStatusCode();
     }
 }
