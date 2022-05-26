75ee9ea14a8b25a14f278e31917040b479244b8e
Oreneile Sejeso <sejesoo@gmail.com>
2022-05-26 23:33:33 +0200

    Autowired UserServiceInterface.

--- src/main/java/springbootpart1/springboot/controller/UserController.java
+++ src/main/java/springbootpart1/springboot/controller/UserController.java
@@ -5,30 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springbootpart1.springboot.dao.FakeRepoInterface;
+import springbootpart1.springboot.service.UserService;
 
 @RequestMapping("user")
 @RestController
 public class UserController {
 
     @Autowired
-    FakeRepoInterface fakeRepo;
+    UserService userService;
 
     @PostMapping(path = "/add/{id}")
     public ResponseEntity<String> addUser(@RequestBody String userName){
-        fakeRepo.insertUser(13,userName.split(" ")[0],userName.split(" ")[1]);
+        userService.addUser(userName.split(" ")[0],userName.split(" ")[1]);
         return new ResponseEntity("Success!", HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/retrieve/{id}")
     public ResponseEntity<String> getUser(@PathVariable(value="id")long userId){
-        return new ResponseEntity(fakeRepo.findUserById(userId), HttpStatus.OK);
+        return new ResponseEntity(userService.getUser(userId), HttpStatus.OK);
 
     }
 
     @DeleteMapping(path = "/remove/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable(value ="id") long userId){
-        fakeRepo.deleteUser(userId);
+        userService.removeUser(userId);
         return new ResponseEntity("Success!", HttpStatus.OK);
     }
 }
